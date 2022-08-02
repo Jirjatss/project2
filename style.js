6e555079d81cb796cb546c4410a2ee02845bc04f
Muhammad Hijir Sajad <sajadhijir@gmail.com>
2022-08-02 14:36:37 +0800

    tugas BMI

---
+++ style.js
@@ -0,0 +1,60 @@
+let okButton = document.getElementById("button");
+let submit = document.getElementById("submit");
+let hasil = document.getElementById("hasil");
+let form = document.getElementById("form");
+let handles = document.getElementsByClassName("handle");
+let submitButton = document.getElementsByClassName("submit-button");
+
+submit.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  const height = parseInt(document.getElementById("height").value);
+  const weight = parseInt(document.getElementById("weight").value);
+
+  if (weight === "" || isNaN(weight) || weight <= 0) {
+    weight_status = false;
+  } else {
+    weight_status = true;
+  }
+
+  if (height === "" || isNaN(height) || height <= 0) {
+    height_status = false;
+  } else {
+    height_status = true;
+  }
+
+  if (height_status && weight_status) {
+    const BMI = weight / (height / 100);
+
+    if (BMI <= 18.5) {
+      hasil.innerHTML = `Your BMI is ${BMI} which means you are Underweight`;
+    } else if (BMI > 18.5 && BMI < 25) {
+      hasil.innerHTML = `Your BMI is ${BMI} which means you are Normal`;
+    } else if (BMI >= 25 && BMI < 30) {
+      hasil.innerHTML = `Your BMI is ${BMI} which means you are Overweight`;
+    } else {
+      hasil.innerHTML = `Your BMI is ${BMI} which means you are Obesity`;
+    }
+  } else {
+    hasil.innerHTML = "Masukkan data yang valid";
+  }
+
+  showAlert();
+});
+
+function showAlert() {
+  okButton.classList.remove("hide");
+  submit.classList.add("hide");
+}
+
+alertButton.addEventListener("click", showAlert);
+
+function handler() {
+  okButton.classList.add("hide");
+  submit.removeEventListener();
+  form.reset();
+}
+
+for (let i = 0; i < handles.length; i++) {
+  handles[i].addEventListener("click", handler);
+}
